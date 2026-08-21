c07fc008d548e0aad55dc78e9a4113db7b00b465
Philippe Laval <philippe.laval@qualiparc.net>
2026-08-21 13:14:48 +0200

    Ajoutez des fichiers projet.

---
+++ CsScriptTest/Program.cs
@@ -0,0 +1,334 @@
+﻿using CSScripting;
+using CSScriptLib;
+using CsScriptTest;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace Client.NET
+{
+    public interface ICalc
+    {
+        int Sum(int a, int b);
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PrepareCodeDomCompilers();
+
+            //Samples.LoadCode();
+            //Samples.LoadFile();
+            //Samples.CreateDelegate();
+            //Samples.LoadCodeWithInterface();
+            Samples.CompileCode();
+            Samples.ScriptReferencingScript();
+            Samples.Referencing();
+
+            TestRoslynEvaluatorLoadMethod();
+            TestEvaluatorCompileCodeAndLoadMethodWithInterface();
+            TestEvaluatorLoadMethodWithInterface();
+            Test_CodeDom();
+            Test_Roslyn();
+
+        }
+
+        private static void TestRoslynEvaluatorLoadMethod()
+        {
+            // In order to be able to debug the code (step inside the code)
+            CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+            CSScript.EvaluatorConfig.DebugBuild = true;
+
+            dynamic script = CSScript.RoslynEvaluator
+                                     .LoadMethod(@"public (int, int) func()
+                                                   {
+                                                       return (0,5);
+                                                   }");
+
+            /*
+             * In debugger F11 (step in) and we will see the code executed :
+public class DynamicClass
+{
+	public (int, int) func()
+	{
+		return (0, 5);
+	}
+}
+             */
+
+            (int, int) result = script.func();
+        }
+
+        private static void TestEvaluatorCompileCodeAndLoadMethodWithInterface()
+        {
+            // In order to be able to debug the code (step inside the code)
+            CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+            CSScript.EvaluatorConfig.DebugBuild = true;
+
+            var info = new CompileInfo { 
+                //RootClass = "Printing",
+                AssemblyFile = "Printer.dll", 
+                AssemblyName = "PrintAsm" 
+            };
+
+            // in debuger, we see that the object printer_asm has one entry in DefinedTypes
+            // Name = "Printer", FullName = "MyGreatApp.Printer"
+
+            if (printer_asm == null)
+                printer_asm = CSScript.Evaluator
+                                      .CompileCode(@"using System;
+                                                     namespace MyGreatApp;
+                                                     public class Printer
+                                                     {
+                                                         public static void Print() =>
+                                                             Console.WriteLine(""Printing..."");
+                                                     }", info);
+
+            var script = CSScript.Evaluator
+                                 .With(eval => eval.IsAssemblyUnloadingEnabled = true)
+                                 .ReferenceAssembly(printer_asm)
+                                 .LoadMethod<ICalc>(@"public int Sum(int a, int b)
+                                                     {
+                                                         MyGreatApp.Printer.Print();
+                                                         return a+b;
+                                                     }");
+
+            // Using the debugger, we step in Sum() to see the generated code below.
+            // We see that :
+            // - using LoadMethod<ICalc>() will generate the "using Client.NET;" line. Its the namespace where the ICalc is defined.
+            // - MyGreatApp.Printer.Print(); will generate "using MyGreatApp;" and "Printer.Print();"
+
+            script.Sum(1, 2);
+            script.GetType().Assembly.Unload();
+
+            /*
+            using Client.NET;
+            using MyGreatApp;
+
+            public class DynamicClass : ICalc
+            {
+                public int Sum(int a, int b)
+                {
+                    Printer.Print();
+                    return a + b;
+                }
+            }
+            */
+
+            // We can then step in the Printer.Print() to see the generated code below.
+
+            /*
+            using System;
+
+            namespace MyGreatApp;
+
+            public class Printer
+            {
+                public static void Print()
+                {
+                    Console.WriteLine("Printing...");
+                }
+            }
+            */
+        }
+
+        static void TestEvaluatorLoadMethodWithInterface()
+        {
+            // In order to be able to debug the code (step inside the code)
+            CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+            CSScript.EvaluatorConfig.DebugBuild = true;
+
+            var script = CSScript.Evaluator
+                                 .With(eval => eval.IsAssemblyUnloadingEnabled = true)
+                                 .LoadMethod<ICalc>(@"public int Sum(int a, int b)
+                                                     { return a+b; }");
+
+
+            /*
+             * In debugger F11 (step in) and we will see the code executed.
+             * We see that .LoadMethod<ICalc> we generate the line "using Client.NET;" because ICalc is defined in this namespace.
+             * 
+using Client.NET;
+
+public class DynamicClass : ICalc
+{
+	public int Sum(int a, int b)
+	{
+		return a + b;
+	}
+}
+             */
+            script.Sum(1, 2);
+
+            script.GetType().Assembly.Unload();
+        }
+
+
+        private static void Test()
+        { 
+            PrepareCodeDomCompilers();
+
+            Console.WriteLine("================\n");
+            Console.WriteLine($"Loading and unloading script 20 times");
+            Test_Unloading();
+            Console.WriteLine("================\n");
+
+            CSScript.StopBuildServer();
+            CSScript.EvaluatorConfig.DebugBuild = true;
+
+            var sw = Stopwatch.StartNew();
+
+            Console.WriteLine($"Hosting runtime: .NET {(Runtime.IsCore ? "Core" : "Framework")}");
+            Console.WriteLine("================\n");
+
+            Console.WriteLine("CodeDOM");
+            Test_CodeDom();
+            Console.WriteLine("  first run: " + sw.ElapsedMilliseconds);
+            sw.Restart();
+            Test_CodeDom();
+            Console.WriteLine("  next run: " + sw.ElapsedMilliseconds);
+
+            Console.WriteLine("\nRoslyn");
+            sw.Restart();
+            Test_Roslyn();
+            Console.WriteLine("  first run: " + sw.ElapsedMilliseconds);
+            sw.Restart();
+            Test_Roslyn();
+            Console.WriteLine("  next run: " + sw.ElapsedMilliseconds);
+        }
+        
+
+        static void Test_CodeDom()
+        {
+            dynamic script = CSScript.CodeDomEvaluator
+                                     .LoadMethod(@"public (int, int) func()
+                                                   {
+                                                       return (0,5);
+                                                   }");
+            (int, int) result = script.func();
+        }
+
+        static void Test_Roslyn()
+        {
+            dynamic script = CSScript.RoslynEvaluator
+                                     .LoadMethod(@"public (int, int) func()
+                                                   {
+                                                       return (0,5);
+                                                   }");
+
+            (int, int) result = script.func();
+        }
+
+        static void call_UnloadAssembly()
+        {
+            CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+            CSScript.EvaluatorConfig.DebugBuild = true;
+
+            var script = CSScript.Evaluator
+                                 .With(eval => eval.IsAssemblyUnloadingEnabled = true)
+                                 .LoadMethod<ICalc>(@"public int Sum(int a, int b)
+                                                     { return a+b; }");
+
+            script.Sum(1, 2);
+
+            script.GetType().Assembly.Unload();
+        }
+
+        static Assembly printer_asm;
+
+        static void call_UnloadAssemblyWithDependency()
+        {
+            var info = new CompileInfo { RootClass = "Printing", AssemblyFile = "Printer.dll", AssemblyName = "PrintAsm" };
+
+            if (printer_asm == null)
+                printer_asm = CSScript.Evaluator
+                                      .CompileCode(@"using System;
+                                                     public class Printer
+                                                     {
+                                                         public static void Print() =>
+                                                             Console.WriteLine(""Printing..."");
+                                                     }", info);
+
+            var script = CSScript.Evaluator
+                                 .With(eval => eval.IsAssemblyUnloadingEnabled = true)
+                                 .ReferenceAssembly(printer_asm)
+                                 .LoadMethod<ICalc>(@"public int Sum(int a, int b)
+                                                     {
+                                                         Printing.Printer.Print();
+                                                         return a+b;
+                                                     }");
+            script.Sum(1, 2);
+            script.GetType().Assembly.Unload();
+        }
+
+        static void call_UnloadAssembly_Failing()
+        {
+            // using 'dynamic` completely breaks CLR unloading mechanism. Most likely it triggers an
+            // accidental referencing of the assembly or System.Runtime.Loader.AssemblyLoadContext.
+            dynamic script = CSScript.Evaluator
+                                     .With(eval => eval.IsAssemblyUnloadingEnabled = true)
+                                     .LoadMethod(@"public int Sum(int a, int b)
+                                                { return a+b; }");
+
+            script.Sum(1, 2);
+
+            (script as object).GetType().Assembly.Unload();
+        }
+
+        static void call_UnloadAssembly_Crashing_CLR()
+        {
+            CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+            CSScript.EvaluatorConfig.DebugBuild = true;
+
+            var script = CSScript.Evaluator
+                                 .With(eval => eval.IsAssemblyUnloadingEnabled = true)
+                                 .LoadMethod<ICalc>(@"public int Sum(int a, int b)
+                                                     { return a+b; }");
+
+            script.Sum(1, 2);
+
+            GC.Collect(); // see https://github.com/oleg-shilo/cs-script/issues/301 for details
+
+            script.GetType().Assembly.Unload();
+        }
+
+        static void Test_Unloading()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Loaded assemblies count: " + AppDomain.CurrentDomain.GetAssemblies().Count());
+
+                call_UnloadAssembly();
+                //call_UnloadAssemblyWithDependency(); // also works OK; provided just for demo
+
+                // call_UnloadAssembly_Failing();
+                // call_UnloadAssembly_Crashing_CLR();
+                GC.Collect();
+            }
+        }
+
+        static void PrepareCodeDomCompilers()
+        {
+            // If you are using CodeDom evaluator and your hosting environment does not have .NET SDK installed
+            // you will need to install the compiler by downloading SDK tools NuGet package.
+            // Either manually from
+            // https://api.nuget.org/v3-flatcontainer/microsoft.net.sdk.compilers.toolset/10.0.103/microsoft.net.sdk.compilers.toolset.10.0.103.nupkg
+            // Or by executing `css -deploy-csc` command in the terminal, which will download and extract the package to the default location.
+            // Or by using CS-Script's own downloader. Uncomment next two lines:
+            //
+            // NugetPackageDownloader.OnProgressOutput = Console.WriteLine;
+            // NugetPackageDownloader.DownloadLatestSdkCompiler(includePrereleases: false);
+            // This sample works even if you do not uncomment the code above because the compiling tools package is added to this
+            // project.
+
+            // Globals.csc is internally initialized the same way. Providing it here for demo purposes only.
+            Globals.csc =
+                Globals.FindSdKCompiler() ?? // or from .NET SDK installed on OS
+                Globals.FindSdkToolsetPackageCompiler(includePrereleases: false); // from the installed Microsoft.Net.Sdk.Compilers.Toolset package
+        }
+    }
+
+    
+}
---
+++ CsScriptTest/Samples.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Xml.Linq;
+using CSScriptLib;
+
+// Important : its indicate where the ICalc and ICalc2 interface are located
+namespace CsScriptTest;
+
+public interface ICalc
+{
+    int Sum(int a, int b);
+}
+
+public interface ICalc2
+{
+    int Sum(int a, int b);
+
+    int Div(int a, int b);
+}
+
+public class Samples
+{
+    public static void LoadCode()
+    {
+        // In order to be able to debug the code (step inside the code)
+        CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+        CSScript.EvaluatorConfig.DebugBuild = true;
+
+        // If we are using a namespace for the interface ICalc (like namespace CsScriptTest in this file)
+        // We MUST indicate "using CsScriptTest;" or the code won't load.
+        // If the interface were not in the namespace like the original file, we do'nt have to use "using CsScriptTest;"
+        dynamic calc = CSScript.Evaluator
+                               .LoadCode(
+                                   @"using System;
+                                     using CsScriptTest;
+                                     public class Script : ICalc
+                                     {
+                                         public int Sum(int a, int b)
+                                         {
+                                             return a+b;
+                                         }
+                                     }");
+        int result = calc.Sum(1, 2);
+    }
+
+    public static void LoadFile()
+    {
+        // In order to be able to debug the code (step inside the code)
+        CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+        CSScript.EvaluatorConfig.DebugBuild = true;
+
+
+        var script = Path.GetTempFileName();
+        try
+        {
+            string content =
+                    """
+                    using System; 
+                    using CsScriptTest;
+
+                    public class Script : ICalc
+                    {
+                        public int Sum(int a, int b)
+                        {
+                            return a + b;
+                        }
+                    }
+                    """;
+
+            File.WriteAllText(script, content);
+
+            dynamic calc = CSScript.Evaluator.LoadFile(script);
+
+            int result = calc.Sum(1, 2);
+        }
+        finally
+        {
+            File.Delete(script);
+        }
+    }
+
+    /*
+    public static void LoadAndUnload()
+    {
+        // Based on https://github.com/dotnet/samples/blob/master/core/tutorials/Unloading/Host/Program.cs
+        //
+        // Limitations: the "accuracy" of the unloading is determined by the quality of the
+        // .NET Core's `AssemblyLoadContext` implementation. For example using variable of `dynamic` type to keep the
+        // reference to the script object is problematic. Thus the loaded assembly file stays locked
+        // even though the runtime successfully reports unloading the assembly and collecting the weak reference
+        // of `AssemblyLoadContext` object.
+        //
+        // However using interfaces (e.g. `ICalc`) or raw Reflection does not exhibit this problem.
+        //
+        // Having a debugger attached to the process can also affect the outcome of the unloading.
+
+        WeakReference assemblyHost;
+
+        var asmFile = Path.GetFullPath("Script.dll");
+
+        Samples.LoadAndUnloadImpl(asmFile, out assemblyHost);
+
+        // Poll and run GC until the AssemblyLoadContext is unloaded.
+        // You don't need to do that unless you want to know when the context
+        // got unloaded. You can just leave it to the regular GC.
+        for (int i = 0; assemblyHost.IsAlive && (i < 10); i++)
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        Console.WriteLine($"Unload success: {!assemblyHost.IsAlive}");
+
+        File.Delete(asmFile); // prove that the assembly is unloaded
+    }
+
+    public static void LoadAndUnloadImpl(string asmFile, out WeakReference alcWeakRef)
+    {
+        CSScript.Evaluator
+                .ReferenceAssemblyOf<ICalc>()
+                .CompileAssemblyFromCode(
+                    @"using System;
+                      public class Script : ICalc
+                      {
+                          public int Sum(int a, int b)
+                          {
+                              return a+b;
+                          }
+                      }", asmFile);
+
+        var asm = new UnloadableAssembly();
+
+        alcWeakRef = new WeakReference(asm);
+
+        ICalc script = (ICalc)asm.LoadFromAssemblyPath(asmFile)
+                                    .CreateObject("*"); // or `CreateInstance("css_root+Script")`
+
+        int result = script.Sum(1, 3);
+
+        asm.Unload();
+    }
+
+    class UnloadableAssembly : AssemblyLoadContext
+    {
+        public UnloadableAssembly(string name = null) : base(name ?? Guid.NewGuid().ToString(), isCollectible: true)
+            => this.Unloading += x => Console.WriteLine("Unloading " + this.Name);
+    }
+
+    public static void LoadMethod()
+    {
+        dynamic script = CSScript.RoslynEvaluator
+                                 .LoadMethod(
+                                     @"int Product(int a, int b)
+                                       {
+                                           return a * b;
+                                       }");
+
+        int result = script.Product(3, 2);
+    }
+
+    public static void LoadMethodWithInterface()
+    {
+        ICalc2 script = CSScript.RoslynEvaluator
+                                .LoadMethod<ICalc2>(
+                                    @"public int Sum(int a, int b)
+                                      {
+                                          return a + b;
+                                      }
+                                      public int Div(int a, int b)
+                                      {
+                                          return a/b;
+                                      }");
+        int result = script.Sum(15, 3);
+    }
+    */
+
+    public static void CreateDelegate()
+    {
+        // In order to be able to debug the code (step inside the code)
+        CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+        CSScript.EvaluatorConfig.DebugBuild = true;
+
+
+        var log = CSScript.RoslynEvaluator
+                          .CreateDelegate(@"void Log(string message)
+                                            {
+                                                Console.WriteLine(message);
+                                            }");
+
+        /*
+         * With debugger (F11), we can see the generated code :
+         * 
+        using System;
+
+        public class DynamicClass
+        {
+            public static void Log(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+        */
+
+        log("Test message");
+    }
+
+    public static void LoadCodeWithInterface()
+    {
+        // In order to be able to debug the code (step inside the code)
+        CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+        CSScript.EvaluatorConfig.DebugBuild = true;
+
+        string code = @"
+                using System;
+                public class Script : ICalc
+                {
+                    public int Sum(int a, int b)
+                    {
+                        return a + b;
+                    }
+                }";
+
+        var script = CSScript.Evaluator.LoadCode<ICalc>(code);
+
+        int result = script.Sum(13, 2);
+    }
+
+    public static void CompileCode()
+    {
+        // In order to be able to debug the code (step inside the code)
+        CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+        CSScript.EvaluatorConfig.DebugBuild = true;
+
+        var info = new CompileInfo { 
+            //RootClass = "printer_script",
+            AssemblyFile = "script.dll" };
+
+        var printer_asm = CSScript.Evaluator
+                                  .CompileCode(
+                                      @"using System;
+                                        public class Printer
+                                        {
+                                            public static void Print() =>
+                                                Console.WriteLine(""Printing..."");
+                                        }", info);
+
+        //var type = printer_asm?.GetType("printer_script+Printer");
+        var type = printer_asm?.GetType("Printer");
+        var method = type?.GetMethod("Print");
+
+        /*
+         * In debugger, with F11, we can step in the generated code :
+        using System;
+
+        public class Printer
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Printing...");
+            }
+        } 
+         */
+
+        method?.Invoke(null, null);
+    }
+
+    public static void ScriptReferencingScript()
+    {
+        // In order to be able to debug the code (step inside the code)
+        CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+        CSScript.EvaluatorConfig.DebugBuild = true;
+
+        // Seems not be used with .net core
+        var info = new CompileInfo { RootClass = "printer_script" };
+
+        // In debugger, the object printer_asm has a property DefinedTypes with 1 entry: 
+        // Name = "Printer", FullName = "MyGreatApp.Printer"
+        // If we don't indicate "namespace MyGreatApp;", the DefinedTypes entry will be :
+        // Name = "Printer", FullName = "Printer"
+
+        var printer_asm = CSScript.Evaluator
+                                  .CompileCode(
+                                      @"using System;
+                                        namespace MyGreatApp;
+                                        public class Printer
+                                        {
+                                            public void Print() =>
+                                                Console.WriteLine(""Printing..."");
+                                        }", info);
+
+        dynamic script = CSScript.Evaluator
+                                 .ReferenceAssembly(printer_asm)
+                                 .LoadMethod(@"void Test()
+                                               {
+                                                   new MyGreatApp.Printer().Print();
+                                               }");
+        
+        script.Test();
+
+        /*
+        using MyGreatApp;
+
+        public class DynamicClass
+        {
+            public void Test()
+            {
+                new Printer().Print();
+            }
+        }
+        */
+
+        /*
+        using System;
+
+        namespace MyGreatApp;
+
+        public class Printer
+        {
+            public void Print()
+            {
+                Console.WriteLine("Printing...");
+            }
+        }
+        */
+    }
+
+    public static void Referencing()
+    {
+        // In order to be able to debug the code (step inside the code)
+        CSScript.EvaluatorConfig.PdbFormat = Microsoft.CodeAnalysis.Emit.DebugInformationFormat.Embedded;
+        CSScript.EvaluatorConfig.DebugBuild = true;
+
+        string code = @"
+                using System;
+                using System.Xml;
+                // Needed if ICalc is defined in the namespace
+                using CsScriptTest;
+
+                public class Script : ICalc
+                {
+                    public int Sum(int a, int b)
+                    {
+                        return a + b;
+                    }
+                }";
+
+        var script = CSScript.Evaluator
+                             .ReferenceAssembliesFromCode(code)
+                             .ReferenceAssembly(Assembly.GetExecutingAssembly())
+                             .ReferenceAssembly(Assembly.GetExecutingAssembly().Location)
+                             .ReferenceAssemblyByName("System")
+                             .ReferenceAssemblyByNamespace("System.Xml")
+                             .TryReferenceAssemblyByNamespace("Fake.Namespace", out var resolved)
+                             .ReferenceAssemblyOf(new Samples())
+                             .ReferenceAssemblyOf<XDocument>()
+                             .ReferenceDomainAssemblies()
+                             .LoadCode<ICalc>(code);
+
+        int result = script.Sum(13, 2);
+
+        /*
+         * Generated code :
+         
+using CsScriptTest;
+
+public class Script : ICalc
+{
+	public int Sum(int a, int b)
+	{
+		return a + b;
+	}
+}
+*/
+    }
+}
